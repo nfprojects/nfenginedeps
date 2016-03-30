c26a5d21afd71f442ebc465682c31ca19e02b5ce
Lukasz Kostyra <l.kostyra@samsung.com>
2016-03-30 13:09:49 +0200

    Remove AntTweakBar
    AntTweakBar was replaced by ImGUI some time ago. It is now unneeded
    in the Deps repository.
    
    Verification:
        Rebuild all Deps.
    
    Change-Id: Ie05233c0bbdbc9ae3d9ae951484beb1be030465f

--- Scripts/clean.sh
+++ Scripts/clean.sh
@@ -16,13 +16,4 @@ find . -name \*.so | xargs rm -f
 # Remove all makefiles, except for AntTweakBar's Makefile
 find . -name Makefile | grep -Ev './AntTweakBar/src/Makefile|./FreeType/tools/ftrandom/Makefile|./squish/src/Makefile' | xargs rm -f
 
-echo "Removing AntTweakBar compilation results"
-pushd . > /dev/null
-cd AntTweakBar
-
-find . -name libAntTweakBar\* | xargs rm -f
-find . -name \*.o | xargs rm -f
-
-popd > /dev/null
-
 popd > /dev/null
